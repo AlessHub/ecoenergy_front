51fb4f334b60bbcad130aa1e1937bdbc7caf03f3
AlessHub <aless312@gmail.com>
2023-02-14 08:55:01 +0100

    made folder structure, created navbar template and public route, installed dependencies

---
+++ src/views/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/layout/navigation/Navbar'
+
+const Home = () => {
+  return (
+    <>
+        <Navbar/>
+        <h1>Holi</h1>
+    </>
+  )
+}
+
+export default Home
